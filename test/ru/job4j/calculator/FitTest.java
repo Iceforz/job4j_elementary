e13af34916c7f9e59908cb603aacd4c4891eb280
iceforz <iceforz3@gmail.com>
2020-06-04 14:58:15 +0300

    2. Тесты для идеальный вес [#296448]

---
+++ test/ru/job4j/calculator/FitTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import ru.job4j.converter.Converter;
+
+import static org.junit.Assert.*;
+
+public class FitTest {
+
+    @Test
+    public void manWeight() {
+        double in = 180;
+        double expected = (in - 100) * 1.15;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out,0.01);
+    }
+
+    @Test
+    public void womanWeight() {
+        double in = 165;
+        double expected = (in - 110) * 1.15;
+        double out = Fit.womanWeight(in);
+        Assert.assertEquals(expected, out,0.01);
+    }
+    
+}
