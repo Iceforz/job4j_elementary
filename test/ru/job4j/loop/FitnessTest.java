54441182ac9525bf707d768fa56f723596b07fbd
iceforz <iceforz3@gmail.com>
2020-06-20 14:54:29 +0300

    Добавьте тест с параметрами (90, 90). Метод должен вернуть 1.

--- test/ru/job4j/loop/FitnessTest.java
+++ test/ru/job4j/loop/FitnessTest.java
@@ -20,6 +20,12 @@ public class FitnessTest {
     }
 
     @Test
+    public void whenIvanequalsByFewNik() {
+        int month = Fitness.calc(90, 90);
+        assertThat(month, is(1));
+    }
+
+    @Test
     public void whenIvanLessByFewNik() {
         int month = Fitness.calc(50, 90);
         assertThat(month, is(2));
